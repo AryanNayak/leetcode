7cf67886b72716753d0efbfb192a79d271ba7d49
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-01-04 14:57:28 +0530

    Time: 1016 ms (12.09%), Space: 270.7 MB (5.08%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -20,33 +20,23 @@ public:
     void twoSumSearch(vector<int>& nums, int i, vector<vector<int>>& ans)
     {
         
-        int low = i + 1, high = nums.size() - 1; 
+        unordered_set<int> mp; 
         
-        while(low < high)
+        for(int j = i + 1; j < nums.size(); j++)
         {
-            int sum = nums[low] + nums[high] + nums[i]; 
+            int complement = - nums[i] - nums[j]; 
             
-            if(sum < 0)
+            if(mp.count(complement))
             {
-                low++; 
-            }
-            else if(sum > 0)
-            {
-                high--; 
-            }
-            else
-            {
+                ans.push_back({nums[i], nums[j], complement});
                 
-                ans.push_back({nums[low++], nums[high--], nums[i]}); 
-                
-                while(low < high && nums[low] == nums[low - 1])
+                while(j + 1 < nums.size() && nums[j] == nums[j + 1])
                 {
-                    low++; 
+                    j++; 
                 }
-                
             }
             
-            
+            mp.insert(nums[j]); 
         }
         
         
