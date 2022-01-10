9d357634edda2f05cf3ab46b30dd836ea79d5871
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-01-10 19:32:11 +0530

    Time: 7 ms, Memory: 8.8 MB - LeetHub

--- find-peak-element/find-peak-element.cpp
+++ find-peak-element/find-peak-element.cpp
@@ -5,24 +5,21 @@ public:
         
         int low = 0, high = nums.size() - 1; 
         
+        
         while(low < high)
         {
-            
             int mid = low + (high - low)/2; 
-            
-            
             
             if(nums[mid] > nums[mid + 1])
             {
-                high = mid; 
+                high = mid;
             }
             else
             {
-                low = mid  +1; 
+                low = mid + 1; 
             }
         }
-    
-        return high; 
                 
+        return high; 
     }
 };
