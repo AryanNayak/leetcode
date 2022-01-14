149597acfeca3c474c221408568b2e40fd23a04a
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-01-14 22:58:35 +0530

    Time: 78 ms (15.91%), Space: 31.5 MB (44.89%) - LeetHub

--- 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.cpp
+++ 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.cpp
@@ -3,11 +3,11 @@ public:
     vector<string> findStrobogrammatic(int n) {
         
         return helper(n, n); 
-        
     }
     
-    vector<string> helper(int n, int m)
+    vector<string> helper(int n, int target)
     {
+        //BASE CASE 
         
         if(n == 0)
         {
@@ -16,34 +16,36 @@ public:
         
         if(n == 1)
         {
-            return {"1", "0", "8"}; 
+            return {"0", "1", "8"}; 
         }
+            
+        //RECURRENCE RELATIONS
         
-        
-        vector<string> list = helper(n - 2, m); 
-        
+        vector<string> list = helper(n - 2, target); //n = 2 -> ""
+                                                    //n = 4 -> {"00", "11", "69", "96"}
         
         vector<string> res; 
         
+
         
-        for(int i = 0; i < list.size(); i++)
+        for(int i =0 ; i < list.size(); i++)
         {
             
+            string s = list[i];  // "" n = 4  
             
-            string s = list[i]; 
-            
-            if(n != m)
+            if(n != target)
             {
-                res.push_back("0" + s + "0"); 
+                res.push_back("0" + s + "0");  
             }
-            
-            res.push_back("1" + s + "1"); 
-            res.push_back("6" + s + "9"); 
-            res.push_back("8" + s + "8"); 
-            res.push_back("9" + s + "6"); 
+        
+            res.push_back("1" + s + "1"); // 11 // 1001 // 1 11 1
+            res.push_back("6" + s + "9"); //69 //6009 // 6 11 9 
+            res.push_back("9" + s + "6"); //96 /9006
+            res.push_back("8" + s + "8"); //88// 8008
+    
         }
         
         return res; 
-           
-    }  
+        
+    }   
 };
