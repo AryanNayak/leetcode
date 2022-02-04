a00f905d12b26599286cae9eca6acb08b71a7e8c
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-02-04 14:16:12 +0530

    Time: 148 ms (49.62%), Space: 32.1 MB (35.85%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+       queue<string> q; 
+        
+       unordered_set<string> ht; 
+        
+        q.push(s); 
+        
+        vector<string> res; 
+        
+        while(q.empty() == false)
+        {
+            
+            string ans = q.front(); 
+            q.pop(); 
+            
+            if(ht.count(ans))
+            {
+                continue; 
+            }
+            
+            
+            ht.insert(ans); 
+            
+            if(isValid(ans))
+            {
+                res.push_back(ans); 
+            }
+            else if(res.empty())
+            {
+                for(int i = 0; i < ans.size(); i++)
+                {
+                    if(ans[i] == ')' || ans[i] == '(')
+                    {
+                        q.push(ans.substr(0, i) + ans.substr(i + 1)); 
+                    }
+                }
+            }
+            
+            
+        }
+        return res; 
+        
+    }
+    
+    bool isValid(string& s)
+    {
+        
+        int count = 0; 
+        
+        for(auto c : s)
+        {
+            if(c == '(')
+            {
+                count++; 
+            }
+            else if(c == ')')
+            {
+                if(count > 0)
+                {
+                    count--; 
+                }
+                else 
+                {
+                    return false; 
+                }
+            }
+        }
+        
+        return !count;    
+    }
+};
