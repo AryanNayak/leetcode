ac539f8f272dba95489c379ad1e1ea8711fac98b
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-02-19 01:27:29 +0530

    Time: 45 ms (18.86%), Space: 12.3 MB (52.32%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+    
+        sort(intervals.begin(), intervals.end()); 
+        
+        priority_queue<int, vector<int>, greater<int>> pq; 
+        
+        for(auto m : intervals)
+        {
+            if(pq.empty() == false && pq.top() <= m[0])
+            {
+                pq.pop(); 
+            }
+            
+            pq.push(m[1]); 
+        }
+        
+
+        
+        return pq.size(); 
+    }
+};
