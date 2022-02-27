b36a17defcf23258b0fb1fe8d4a497fd3f3a1793
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-02-27 21:06:06 +0530

    Time: 88 ms (69.20%), Space: 173.1 MB (74.83%) - LeetHub

--- 428-serialize-and-deserialize-n-ary-tree/428-serialize-and-deserialize-n-ary-tree.cpp
+++ 428-serialize-and-deserialize-n-ary-tree/428-serialize-and-deserialize-n-ary-tree.cpp
@@ -20,94 +20,61 @@ public:
 
 class Codec {
 public:
- string serialize(Node* root) {
-        string res; 
-        if (!root) return "[null]"; 
-        
-        // BFS
-        queue<Node*> q; 
+  // Encodes a tree to a single string.
+    string serialize(Node* root) {
+        if (!root)
+            return "";
+        ostringstream ostr;
+        queue<Node*> q;
         q.push(root);
-        while (!q.empty())
-        {
-            int sz=q.size();
-            for (int i=0; i<sz; i++)
-            {
-                Node* cur= q.front(); q.pop();
-                // NULL node
-//                 if (!root) { res+="null ";  continue; } 
-                
-                res+=to_string(cur->val);
-                res.push_back(' ');
-                
-                // number of children
-                int c_count = cur->children.size();
- // do we really need to push count? yes! , otherwise don't know how many children to parse later!!              
-                res+= to_string(c_count); 
-                res.push_back(' ');
-
-                for (int j=0; j<c_count; j++)
-                    q.push(cur->children[j]);
-            }
-        }    
-        // mistake: forget adding []
-        res="["+res+"]";
-       //  cout<<res<<endl;
-        return res; 
-    }
-	
-    // Decodes your encoded data to tree.
-    Node* deserialize(string data) {        
-        //strip off []
-        data = data.substr(1, data.size()-2);        
-        return parse(data);     
-    }
-
-    Node* parse (string & data)
-    {   
-        istringstream iss(data);
-        queue<Node*> q;  Node* root; 
-         // obtain the first node, null or an integer
-        string str;  iss>> str; 
-        
-        if (str=="null")  return NULL;  
-       
-        root= new Node (stoi(str)); // value        
-        int c_count; iss>> c_count;  // child count
-        root->children.resize(c_count);
-        q.push(root); // push the root node        
-        
-        bool isRoot=true;        
-        
-        while (!q.empty())
-        {
-            int sz=q.size();
-            for (int i=0; i<sz; i++)
-            {
-                //  current node's value
-                Node* cur = q.front(); q.pop();                
-               
-                // number of children
-                for (int j=0; j<cur->children.size(); j++)
-                {   
-                    string str; iss>>str;                
-                    if (str=="null") {cur->children[j]=NULL; continue; } 
-                    // value
-                    // mistake: !! int val; iss>> val;// str is already value now
-                    Node* child = new Node(stoi(str)); 
-                                       
-                    // problem: first root node has value and children count set already                
-                    // solution: shift the logic: when first creating the node, set all its values                                       
-                    int c_count; iss>>c_count;                 
-                    child->children.resize(c_count); 
-                    
-                    // attach child to current parent
-                    cur->children[j]=child; 
-                    
+        while (!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            // if (curr) {
+                ostr << curr->val << " " << curr->children.size() << " ";
+                for (auto child : curr->children) {
                     q.push(child);
                 }
-            }   
+            // } else {
+            //     ostr << "# ";
+            // }
         }
-        return root; 
+        return ostr.str();
+    }
+
+    // Decodes your encoded data to tree.
+    Node* deserialize(string data) {
+        if (data.empty())
+            return nullptr;
+        istringstream istr(data);
+        queue<Node*> q;
+        string val = "", size = "";
+        istr >> val;
+        istr >> size;
+        Node* root = new Node(stoi(val), vector<Node*>(stoi(size), nullptr)), *node = root;
+        q.push(node);
+        bool done = false;
+        while (!q.empty() && !done) {
+            auto curr = q.front();
+            q.pop();
+            int s = curr->children.size();
+            for (int i = 0; i < s; i++) {
+                if (!(istr >> val)) {
+                    done = true;
+                    break;
+                }
+                if (!(istr >> size)) {
+                    done = true;
+                    break;
+                }
+                if (val != "#") {
+                    Node* next = new Node(stoi(val), vector<Node*>(stoi(size), nullptr));
+                    curr->children[i] = next;
+                    q.push(next);
+                }
+            }
+        }
+        return root;
     }
 };
 
