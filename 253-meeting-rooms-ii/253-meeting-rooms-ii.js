39601c04de047cd365a5f11f7c68b58c229a7dc0
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-03-01 10:11:30 +0530

    Time: 80 ms (90.58%), Space: 44.9 MB (24.99%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function(intervals) {
+    if (!intervals || intervals.length < 1) {
+        return 0;
+    }
+    let rooms = 0;
+    let end = 0;
+    const starts = intervals.map(a => a[0]).sort((a, b) => a-b);
+    const ends = intervals.map(a => a[1]).sort((a, b) => a-b);
+    
+    console.log(starts)
+    
+    console.log(ends)
+    
+    for(let i=0; i < intervals.length; i++) {
+        if(starts[i] < ends[end]) {
+            rooms++;
+        } else {
+            end++;
+        }
+    }
+    return rooms;
+};
