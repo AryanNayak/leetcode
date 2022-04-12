a028f937b5f345af0fbefe5d17399981a124a83d
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-04-12 23:31:22 +0530

    Time: 7 ms (51.69%), Space: 6.7 MB (70.59%) - LeetHub

--- 772-basic-calculator-iii/772-basic-calculator-iii.cpp
+++ 772-basic-calculator-iii/772-basic-calculator-iii.cpp
@@ -1,35 +1,130 @@
 class Solution {
 public:
-
-      int calculate(string s) {
-        int i = 0;
-        return parseExpr(s, i);
-    }
     
-    int parseExpr(string& s, int& i) {
-        vector<int> nums;
-        char op = '+';
-        for (; i < s.length() && op != ')'; i++) {
-            if (s[i] == ' ') continue;
-            long n = s[i] == '(' ? parseExpr(s, ++i) : parseNum(s, i);
-            switch(op) {
-                case '+' : nums.push_back(n); break;
-                case '-' : nums.push_back(-n); break;
-                case '*' : nums.back() *= n; break;
-                case '/' : nums.back() /= n; break;
-            }            
-            if (i < s.length())                  // handle over flow of i in cases like "1 + 1"
-                op = s[i];
+    int prescedence(char ch)
+    {
+        if(ch == '*' || ch == '/')
+        {
+            return 3;
+        } 
+        else if(ch == '+' || ch == '-')
+        {
+            return 2; 
         }
-        for(auto m : nums)
-        cout << m << endl; 
-        return accumulate(nums.begin(), nums.end(), 0);
+        else
+        {
+            return 1; 
+        }
     }
     
-    long parseNum(string& s, int& i) {            // change to long to pass case like "0-2147483648"
-        long n = 0;
-        while(i < s.length() && isdigit(s[i]))
-            n = s[i++] - '0' + 10 * n;
-        return n;
+    
+    int calc(char ch, int a, int b)
+    {
+        if(ch == '*')
+        {
+            return a*b; 
+        }
+        else if(ch == '/')
+        {
+            return b/a; 
+        }
+        else if(ch == '-')
+        {
+            return b - a; 
+        }
+        
+        return a + b; 
+    }
+    int process(string s)
+    {
+        
+        stack<int> stk; 
+        
+        for(int i = 0; i < s.size();)
+        {
+            if(s[i] == ',')
+            {
+                i++; 
+                continue;
+            }
+            else if(s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/')
+            {
+                int a = stk.top(); 
+                stk.pop(); 
+                int b = stk.top(); 
+                stk.pop(); 
+                stk.push(calc(s[i], a, b)); 
+                i++; 
+            }
+            else if(isalnum(s[i]))
+            {
+                string temp; 
+                while(i < s.size() && isalnum(s[i]))
+                {
+                    temp = temp + s[i]; 
+                    i++; 
+                }
+                stk.push(stoi(temp)); 
+            }
+            else
+            {
+                i++; 
+            }
+        }
+
+        return stk.top(); 
+    }
+    int calculate(string s) {
+        
+        string postfix_expression = ""; 
+        
+        stack<char> stk; 
+        
+        for(int i =0; i < s.size(); )
+        {
+            char ch = s[i]; 
+            if(isalnum(ch))
+            {
+                while(i < s.size() && isalnum(s[i]))
+                {
+                    postfix_expression += s[i]; 
+                    i++; 
+                }
+                postfix_expression += ','; 
+            }
+            else if(ch == '(')
+            {
+                stk.push(ch); 
+                i++; 
+            }
+            else if(ch == ')')
+            {
+                while(!stk.empty() && stk.top() != '(')
+                {
+                    postfix_expression += stk.top(); 
+                    stk.pop(); 
+                }
+                stk.pop(); 
+                i++; 
+            }else
+            {
+                while(!stk.empty() && prescedence(stk.top()) >= prescedence(ch))
+                {
+                    postfix_expression += stk.top(); 
+                    stk.pop(); 
+                }
+                stk.push(ch); 
+                i++; 
+            }
+        }
+        
+        while(!stk.empty())
+        {
+            postfix_expression += stk.top(); 
+            stk.pop(); 
+        }
+        
+        int ans = process(postfix_expression); 
+        return ans; 
     }
 };
