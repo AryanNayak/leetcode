745c6f76462e1062d9332baeb3cc2d2ac13f875c
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-06-16 10:56:53 +0530

    Time: 36 ms (74.96%), Space: 7.1 MB (92.20%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,57 +1,32 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-                
-        int count = 0; 
-        int n = s.length(); 
-        vector<vector<bool>> dp(n, vector<bool>(n, false)); 
         
-        int start = 0; 
-        int len = 0; 
+        int len = 0, start = 0; 
         
-        for(int g = 0 ; g < n; g++)
+        for(int i = 0; i < s.size(); i++)
         {
-            for(int i = 0, j = g; j < n; j++, i++)
-            {
-                
-                if(g == 0)
-                {
-                    dp[i][j] = true; 
-                }
-                else if(g == 1)
-                {
-                    if(s[i] == s[j])
-                    {
-                        dp[i][j] = true;
-                    }
-                    else
-                    {
-                        dp[i][j] = false; 
-                    }
-                }
-                else
-                {
-                    if(s[i] == s[j] && dp[i + 1][j - 1] == true)
-                    {
-                        dp[i][j] = true; 
-                    }
-                    else
-                    {
-                        dp[i][j] = false; 
-                    }
-                }
-                
-                if(dp[i][j] == true)
-                {
-                    len = g + 1; 
-                    start = i; 
-                }
-                    
-            }
-        }
-        
             
+            extend(s, i, i, len, start); 
+            extend(s, i, i + 1, len, start); 
+        }
         return s.substr(start, len); 
     }
+    
+    void extend(string& s, int left, int right, int& len, int& start)
+    {
+     
+        while(left >= 0 && right <= s.size() - 1 && s[left] == s[right])
+        {
+            left--; 
+            right++; 
+        }
+        
+        
+        if(right - left - 1 > len)
+        {
+            start = left + 1; 
+            len = right - left - 1; 
+        }
+    }
 };
-
