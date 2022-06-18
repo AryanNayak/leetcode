4284e4caf48286f633b5ee5b3d020b18eec823f6
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-06-19 00:23:52 +0530

    Time: 3 ms (47.33%), Space: 10.9 MB (12.90%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        
+        stack<char> stack; 
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == ']')
+            {
+                string decodedString = ""; 
+                while(stack.top() != '[')
+                {
+                    decodedString += stack.top(); 
+                    stack.pop(); 
+                }
+                
+                stack.pop(); 
+                int base = 1; 
+                int k = 0; 
+                
+                while(!stack.empty() && isdigit(stack.top()))
+                {
+                    k = k + (stack.top() - '0')* base; 
+                    stack.pop(); 
+                    base *= 10; 
+                }
+                int currentLen = decodedString.size(); 
+                while(k != 0)
+                {
+                    for(int j = decodedString.size() -1; j >= 0 ; j--)
+                    {
+                        stack.push(decodedString[j]);
+                    }
+                    k--;
+                }    
+            }
+            else
+            {
+                stack.push(s[i]); 
+            }
+        }
+        
+        string result; 
+        for(int i = stack.size() -1; i>=0; i--)
+        {
+            result = stack.top() + result; 
+            stack.pop(); 
+        }
+        
+        return result; 
+        
+        
+        
+        
+    }
+};
