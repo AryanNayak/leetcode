42e918cc9cd86b362c58b7417f2f4dd256ef8083
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-06-19 10:16:18 +0530

    Time: 35 ms (99.44%), Space: 23.3 MB (98.93%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -2,32 +2,29 @@ class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         
+        int n = searchWord.size(); 
+        
         sort(products.begin(), products.end()); 
         
-        int n = searchWord.size(); 
         
         vector<vector<string>> res(n, vector<string>{}); 
         
-        
         for(auto& product : products)
         {
-
-            for(int i = 0; i < n; i++)
+            for(int i = 0; i <  n; i++)
             {
                 if(product[i] != searchWord[i])
+                {
                     break; 
+                }
                 
                 if(product[i] == searchWord[i] && res[i].size() < 3)
                 {
                     res[i].push_back(product); 
-                }                
+                }        
             }
-
         }
         
         return res; 
-        
-        
-        
     }
 };
