2b545a776d53df1fd6f9361cc2a9f62be0c88c9f
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-06-21 09:03:20 +0530

    Time: 32 ms (51.46%), Space: 25.9 MB (88.60%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,45 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* buildTree(int isi, int iei, vector<int>& inorder, int psi, int pei, vector<int>& postorder)
-    {
-        if(psi > pei)
-        {
-            return nullptr;
-        }
-        
-        TreeNode* root = new TreeNode(postorder[pei]); 
-        
-        int idx = isi; 
-        
-        while(inorder[idx] != postorder[pei])
-        {
-            idx++; 
-        }
-        
-        int tel = idx - isi; 
-        
-        
-        root->left = buildTree(isi, idx - 1, inorder, psi, psi + tel - 1, postorder); 
-        root->right = buildTree(idx + 1, iei, inorder, psi + tel, pei - 1, postorder); 
-        
-        return root; 
-    }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
-        int n  = inorder.size(); 
-        
-        return buildTree(0, n - 1, inorder, 0, n - 1, postorder); 
-        
+        int n = size(inorder), postIdx = n-1;
+        return build(inorder, postorder, 0, n-1, postIdx);
+    }
+
+    TreeNode* build(vector<int>& in, vector<int>& post, int inStart, int inEnd, int& postIdx) {
+        if(inStart > inEnd) return nullptr;
+        TreeNode* root = new TreeNode(post[postIdx--]);
+        int inIdx = find(begin(in), end(in), root -> val) - begin(in);
+        root -> right = build(in, post, inIdx+1, inEnd, postIdx);
+        root -> left  = build(in, post, inStart, inIdx-1, postIdx);
+        return root;
     }
 };
