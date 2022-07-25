7987abf0dad0c564dbd165098d049dc3fff54507
AryanNayak <58552613+AryanNayak@users.noreply.github.com>
2022-07-26 03:24:06 +0530

    Time: 12 ms (60.23%), Space: 13.7 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     int bSearchL(vector<int>& nums, int target)
     {
-        
         int low = 0, high = nums.size() - 1; 
-        
         
         while(low <= high)
         {
             
-            int mid = low + (high - low)/2; 
+            int mid = (low + high)/2; 
             
-            if(nums[mid] < target)
-            {
+            
+            if(nums[mid] < target){
                 low = mid + 1; 
             }
             else if(nums[mid] > target)
             {
-                high = mid - 1;
+                high = mid - 1; 
             }
             else
             {
                 
-                if(mid == 0 || nums[mid - 1] != nums[mid])
+                if(mid == 0 || nums[mid] != nums[mid - 1])
                 {
                     return mid; 
                 }
@@ -30,56 +28,50 @@ public:
                 {
                     high = mid - 1; 
                 }
-   
             }
-   
         }
+        return -1; 
+     
         
-        return -1;  
     }
     int bSearchR(vector<int>& nums, int target)
     {
         
         int low = 0, high = nums.size() - 1; 
         
-        
         while(low <= high)
         {
             
-            int mid = low + (high - low)/2; 
+            int mid = (low + high)/2; 
             
             
-            if(nums[mid] < target)
-            {
+            if(nums[mid] < target){
                 low = mid + 1; 
             }
             else if(nums[mid] > target)
             {
                 high = mid - 1; 
             }
-            else 
+            else
             {
                 
-                if(mid == nums.size() - 1 || nums[mid + 1] != nums[mid])
+                if(nums.size() - 1  == mid || nums[mid] != nums[mid + 1])
                 {
                     return mid; 
                 }
-                else 
+                else
                 {
-                    low = mid  + 1; 
+                    low = mid + 1; 
                 }
-  
             }
-     
         }
-            return -1;    
+        return -1; 
+        
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         
         int left = bSearchL(nums, target); 
-        int right = bSearchR(nums, target); 
-        
+        int right = bSearchR(nums, target);
         return {left, right}; 
-        
     }
 };
